c017a978a7168c94822780474dbaf18fcd350d0a
Benoît Burgener <benoit.burgener@urturn.com>
2013-11-24 12:00:27 +0100

    Replace simple text by fraction symbol

---
+++ app/assets/javascripts/ingredients.js
@@ -0,0 +1,30 @@
+(function(jQuery) {
+
+  jQuery(document).on('keyup', '.recipe-amount-field', function(e) {
+    var val = jQuery(this).val(),
+        replacement = '';
+
+    switch(val) {
+      case '1/2':
+        replacement = '½';
+        break;
+      case '1/3':
+        replacement = '⅓';
+        break;
+      case '2/3':
+        replacement = '⅔';
+        break;
+      case '1/4':
+        replacement = '¼';
+        break;
+      case '3/4':
+        replacement = '¾';
+        break;
+    }
+
+    if (replacement) {
+      jQuery(this).val(replacement);
+    }
+  });
+
+})(jQuery);
