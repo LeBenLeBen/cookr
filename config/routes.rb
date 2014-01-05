4af27fc49c14f18dff2854d052ee914f4e84cd01
Benoît Burgener <benoit.burgener@gmail.com>
2014-01-05 16:54:29 +0100

    Disable user registration but allow profile edition

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Cookr::Application.routes.draw do
 
-  devise_for :users
+  # Disable user registration but allow profile update
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
   resources :recipes
   resources :tags, :only => [:index, :show]
