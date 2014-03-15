544d53d7424452059f9cc2679fc7ce512376999e
Benoît Burgener <benoit.burgener@gmail.com>
2014-03-15 17:44:23 +0100

    Set locale depending on HTTP_ACCEPT_LANGUAGE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  def set_locale
+    if I18n.available_locales.include? extract_locale_from_accept_language_header.to_sym
+      I18n.locale = extract_locale_from_accept_language_header
+    end
+  end
+
+  private
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+
 end
