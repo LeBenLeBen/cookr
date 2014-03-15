5195979983ade462be4b0c9856b4e4647e3147bd
Benoît Burgener <benoit.burgener@gmail.com>
2014-03-15 17:50:21 +0100

    Show latest seen recipe first

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     ids = Recipe.pluck(:id)
     @random_recipe = Recipe.find(ids.sample) if ids.present?
-    @last_seen_recipes = Recipe.find(session[:recipe_history]) if session[:recipe_history].present?
+    @last_seen_recipes = Recipe.find(session[:recipe_history]).reverse! if session[:recipe_history].present?
     @recipes = Recipe.last(3).reverse!
   end
 
