522473b455ff70c2a52fa0df2d3f3c246488a372
Benoît Burgener <benoit.burgener@gmail.com>
2015-01-14 19:41:26 +0100

    Update DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,21 +13,18 @@
 
 ActiveRecord::Schema.define(version: 20150103102542) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "ingredients", force: true do |t|
     t.integer  "recipe_id"
     t.text     "amount"
     t.text     "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipes", force: true do |t|
     t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.text     "steps"
     t.integer  "time"
     t.string   "notes"
@@ -45,8 +42,8 @@ ActiveRecord::Schema.define(version: 20150103102542) do
 
   create_table "tags", force: true do |t|
     t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "recipe_count", default: 0
   end
 
@@ -61,11 +58,11 @@ ActiveRecord::Schema.define(version: 20150103102542) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
