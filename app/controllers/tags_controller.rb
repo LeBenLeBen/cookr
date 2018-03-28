3b88b024e53bfc8263bb22ee7375302cc8bdff01
Benoît Burgener <benoit.burgener@gmail.com>
2018-03-28 18:37:18 +0200

    Remove unused tags index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,17 +5,4 @@ class TagsController < ApplicationController
     @recipes = @tag.recipes.alphabetically
   end
 
-  def index
-    respond_to do |format|
-      format.json do
-        query = params[:q].downcase
-        if query.present?
-          render :json => Tag.where("lower(title) like ?", "%#{query}%")
-        else
-          render :json => Tag.all
-        end
-      end
-    end
-  end
-
 end
