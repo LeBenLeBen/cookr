fccc128440ebe69bd5af57bf916c433a1d145a34
Benoît Burgener <benoit.burgener@gmail.com>
2019-12-22 16:30:18 +0100

    Extract CSS with HMR in development to prevent FOUC

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,15 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const environment = require('./environment')
+const environment = require('./environment');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = environment.toWebpackConfig()
+environment.loaders.get('sass').use[0] = {
+  loader: MiniCssExtractPlugin.loader,
+  options: {
+    hmr: true,
+  },
+};
+
+environment.plugins.get('MiniCssExtract').options.filename = 'css/[name].css';
+
+module.exports = environment.toWebpackConfig();
